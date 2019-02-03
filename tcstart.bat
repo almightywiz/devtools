2250941780efb5fab9fdec5b21ef9dff547f42bb
Charles Lukas <charles.lukas@gmail.com>
2019-02-02 23:00:08 -0600

    adding aliases for `edit` shortcuts

--- tcstart.bat
+++ tcstart.bat
@@ -5,3 +5,5 @@ alias tcstart	= `C:\dev\devtools\tcstart.bat`
 alias clear		= `cls /c`
 alias ls		= `dir /b`
 alias open		= `explorer .`
+alias edit		= `notepad++ %1`
+alias tcalias	= `edit C:\dev\devtools\tcstart.bat`
